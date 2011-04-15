aeb30edd2727456d8b85088c048091b4779b7585
Lars Magne Ingebrigtsen <larsi@quimbies.gnus.org>
2011-04-15 03:27:33 +0200

    initial

---
+++ scan.el
@@ -0,0 +1,32 @@
+;;; scan.el --- Editing Sound Files
+;; Copyright (C) 2001, 2002, 2003, 2010, 2011 Lars Magne Ingebrigtsen
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+;; Keywords: music
+
+;; This file is not part of GNU Emacs.
+
+;; Scan is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; Scan is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with Scan; see the file COPYING.  If not, write to the Free
+;; Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+;; 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl)
+
+(provide 'scan)
+
+;;; scan.el ends here
